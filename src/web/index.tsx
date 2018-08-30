43846c0d2ec87ed39789fe231172331f2a51032e
Frederick Gnodtke <frederick@gnodtke.net>
2018-08-30 18:26:13 +0200

    Setup dependency injection in frontend

--- src/web/index.tsx
+++ src/web/index.tsx
@@ -1,5 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { TSDI } from "tsdi";
+import { configureController } from "hyrest";
+import { allControllers } from "../common";
+
+configureController(allControllers, { baseUrl: "http://localhost:4000" });
+
+const tsdi = new TSDI();
+tsdi.enableComponentScanner();
 
 ReactDOM.render(
     <div>
